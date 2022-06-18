d5ceaf06bbb1c8473697c47c56e55a248fb7a040
Aditya Anand <adianand2002@gmail.com>
2022-06-19 00:28:16 +0530

    added select date feature

--- lib/Pages/todo_list_page.dart/add_task.dart
+++ lib/Pages/todo_list_page.dart/add_task.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 import '../../widgets/themes.dart';
 
-class AddTask extends StatelessWidget {
+class AddTask extends StatefulWidget {
   const AddTask({Key? key}) : super(key: key);
+
+  @override
+  State<AddTask> createState() => _AddTaskState();
+}
+
+class _AddTaskState extends State<AddTask> {
+  late DateTime? myDateTime;
+  String date = '';
+  String todayDate = DateFormat('dd-MM-yy').format(DateTime.now());
 
   @override
   Widget build(BuildContext context) {
@@ -21,8 +31,8 @@ class AddTask extends StatelessWidget {
             alignment: Alignment.centerRight,
             child: const Icon(
               CupertinoIcons.xmark_circle,
-              size: 45,
-              color: Vx.gray500,
+              size: 43,
+              color: Vx.gray400,
             )),
         const SizedBox(
           height: 50,
@@ -42,6 +52,97 @@ class AddTask extends StatelessWidget {
                   color: Colors.black26,
                 ),
               )),
+        ),
+        const SizedBox(
+          height: 40,
+        ),
+        Container(
+          padding: const EdgeInsets.only(left: 51),
+          child: Row(
+            children: [
+              InkWell(
+                borderRadius: BorderRadius.circular(25),
+                onTap: () async {
+                  myDateTime = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2010),
+                      lastDate: DateTime(2025));
+
+                  setState(() {
+                    date = DateFormat('dd-MM-yy').format(myDateTime!);
+                  });
+                },
+                child: Container(
+                  width: 135,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Vx.gray400),
+                      borderRadius: BorderRadius.circular(25)),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(
+                          Icons.calendar_today_outlined,
+                          color: Vx.gray500,
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          date == '' || date == todayDate ? 'Today' : date,
+                          style: const TextStyle(
+                              fontSize: 15,
+                              color: Vx.gray500,
+                              fontWeight: FontWeight.bold),
+                        )
+                      ]),
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Container(
+                width: 44,
+                height: 48,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Vx.gray400),
+                    shape: BoxShape.circle),
+                child: Checkbox(
+                  checkColor: Colors.white,
+                  fillColor: MaterialStateProperty.all(Vx.blue600),
+                  value: true,
+                  shape: const CircleBorder(),
+                  onChanged: (bool? value) {},
+                ),
+              )
+            ],
+          ),
+        ),
+        const SizedBox(height: 100),
+        Container(
+          margin: const EdgeInsets.only(left: 130),
+          child: Row(
+            children: const [
+              Icon(Icons.folder_open_sharp, size: 25, color: Vx.gray500),
+              SizedBox(
+                width: 35,
+              ),
+              Icon(
+                Icons.dark_mode_outlined,
+                size: 25,
+                color: Vx.gray500,
+              ),
+              SizedBox(
+                width: 35,
+              ),
+              Icon(
+                Icons.flag_outlined,
+                size: 25,
+                color: Vx.gray500,
+              )
+            ],
+          ),
         )
       ])),
     );
