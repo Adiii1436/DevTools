bf4ecfc3a6fe2957b6dbd9b2b84fdb9145d1af3d
Aditya Anand <adianand2002@gmail.com>
2022-06-28 00:27:21 +0530

    corrected calculator

--- lib/Pages/calculator_page/calculator_page.dart
+++ lib/Pages/calculator_page/calculator_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 class CalculatorPage extends StatefulWidget {
   const CalculatorPage({Key? key}) : super(key: key);
@@ -10,6 +11,7 @@ class CalculatorPage extends StatefulWidget {
 class _CalculatorPageState extends State<CalculatorPage> {
   TextEditingController task = TextEditingController();
   String conc = '';
+  var sum = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -33,119 +35,106 @@ class _CalculatorPageState extends State<CalculatorPage> {
       '00',
       '0',
       '.',
-      '='
+      '=',
+      '(',
+      ')',
+      'e',
+      'π'
     ];
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: [
-          Container(
-            alignment: Alignment.bottomRight,
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height * 0.39,
-            child: Container(
-              padding: const EdgeInsets.only(left: 10, right: 15, bottom: 40),
-              child: TextField(
-                controller: task,
-                scrollPhysics: const BouncingScrollPhysics(),
-                textAlign: TextAlign.right,
-                cursorColor: Colors.black38,
-                maxLines: null,
-                style: const TextStyle(fontSize: 35, color: Colors.black),
-                decoration: const InputDecoration(
-                  // isDense: true,
-                  border: InputBorder.none,
-                  enabled: false,
-                  focusedBorder: InputBorder.none,
-                  enabledBorder: InputBorder.none,
-                  disabledBorder: InputBorder.none,
+          child: Container(
+        margin: const EdgeInsets.only(bottom: 25),
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.bottomRight,
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.39,
+              child: Container(
+                padding: const EdgeInsets.only(left: 10, right: 15, bottom: 40),
+                child: TextField(
+                  controller: task,
+                  scrollPhysics: const BouncingScrollPhysics(),
+                  textAlign: TextAlign.right,
+                  cursorColor: Colors.black38,
+                  maxLines: null,
+                  style: const TextStyle(fontSize: 30, color: Colors.black),
+                  decoration: const InputDecoration(
+                    // isDense: true,
+                    border: InputBorder.none,
+                    enabled: false,
+                    focusedBorder: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    disabledBorder: InputBorder.none,
+                  ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            child: Container(
-                padding: const EdgeInsets.only(left: 10, right: 10),
-                child: GridView.builder(
-                    itemCount: 20,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 4),
-                    itemBuilder: (context, index) {
-                      return InkWell(
-                        onTap: () {
-                          if (text[index] == 'C') {
-                            conc = '';
-                            task = TextEditingController(text: conc);
-                            setState(() {});
-                          } else if (text[index] == '<') {
-                            conc = conc.substring(0, conc.length - 1);
-                            task = TextEditingController(text: conc);
-                            setState(() {});
-                          } else if (text[index] == '=') {
-                            try {
-                              var s1 = conc.split('×');
-                              var sum =
-                                  double.parse(s1[0]) * double.parse(s1[1]);
-                              conc = sum.toString();
+            Expanded(
+              child: Container(
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  child: GridView.builder(
+                      physics: const BouncingScrollPhysics(),
+                      itemCount: 24,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 4),
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                          borderRadius: BorderRadius.circular(10),
+                          onTap: () {
+                            if (text[index] == 'C') {
+                              conc = '';
                               task = TextEditingController(text: conc);
                               setState(() {});
-                            } catch (e) {
+                            } else if (text[index] == '<') {
+                              if (conc.isNotEmpty) {
+                                conc = conc.substring(0, conc.length - 1);
+                                task = TextEditingController(text: conc);
+                                setState(() {});
+                              }
+                            } else if (text[index] == '=') {
+                              conc = conc.replaceAll('×', '*');
+                              conc = conc.replaceAll('÷', '/');
+                              conc = conc.replaceAll('π', '3.141592');
+                              conc = conc.replaceAll('e', '2.718281828');
                               try {
-                                var s1 = conc.split('+');
-                                var sum =
-                                    double.parse(s1[0]) + double.parse(s1[1]);
-                                conc = sum.toString();
+                                ContextModel cm = ContextModel();
+                                Parser p = Parser();
+                                Expression exp = p.parse(conc);
+                                conc = exp
+                                    .evaluate(EvaluationType.REAL, cm)
+                                    .toString();
                                 task = TextEditingController(text: conc);
                                 setState(() {});
                               } catch (e) {
-                                try {
-                                  var s1 = conc.split('-');
-                                  var sum =
-                                      double.parse(s1[0]) - double.parse(s1[1]);
-                                  conc = sum.toString();
-                                  task = TextEditingController(text: conc);
-                                  setState(() {});
-                                } catch (e) {
-                                  try {
-                                    var s1 = conc.split('%');
-                                    var sum = double.parse(s1[0]) %
-                                        double.parse(s1[1]);
-                                    conc = sum.toInt().toString();
-                                    task = TextEditingController(text: conc);
-                                    setState(() {});
-                                  } catch (e) {
-                                    var s1 = conc.split('÷');
-                                    var sum = double.parse(s1[0]) /
-                                        double.parse(s1[1]);
-                                    conc = sum.toString();
-                                    task = TextEditingController(text: conc);
-                                    setState(() {});
-                                  }
-                                }
+                                conc = 'invalid';
+                                task = TextEditingController(text: conc);
+                                setState(() {});
                               }
+                            } else {
+                              conc = conc + text[index];
+                              task = TextEditingController(text: conc);
+                              setState(() {});
                             }
-                          } else {
-                            conc = conc + text[index];
-                            task = TextEditingController(text: conc);
-                            setState(() {});
-                          }
-                        },
-                        child: Container(
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(10)),
-                          margin: const EdgeInsets.all(2),
-                          child: Center(
-                              child: Text(
-                            text[index],
-                            style: const TextStyle(fontSize: 29),
-                          )),
-                        ),
-                      );
-                    })),
-          ),
-        ],
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10)),
+                            margin: const EdgeInsets.all(2),
+                            child: Center(
+                                child: Text(
+                              text[index],
+                              style: const TextStyle(fontSize: 29),
+                            )),
+                          ),
+                        );
+                      })),
+            ),
+          ],
+        ),
       )),
     );
   }
